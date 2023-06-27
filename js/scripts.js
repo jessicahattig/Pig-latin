f74a6dd06cbebd33edac5cf3e183b4c49ad259fb
jessicahattig <jessicahattig@gmail.com>
2023-06-26 20:44:19 -0700

    Trying to push.

--- js/scripts.js
+++ js/scripts.js
@@ -70,9 +70,12 @@ function wordsQu(word) {
 function handleFormSubmission() {
   event.preventDefault();
   const passage = document.getElementById("text-passage").value;
-  const vowelCount = vowelCounter(passage);
-  document.getElementById("output").innerText = vowelCount;
-
+  const vowelCount = vowelFunction(passage);
+  const wayAddition = wayFunction(passage);
+  const consAddition = consenants(passage);
+  document.getElementById("vowel-count").innerText = vowelCount;
+  document.getElementById("way-output").innerText = wayAddition;
+  document.getElementById("consenant-ouput").innerText = consAddition;
 }
 
 window.addEventListener("load", function () {
